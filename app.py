9df23061b8994c318d1f1a32c77e74bb84fcac8e
Sheeraz Zaidi <sheeraz@Sheerazs-MacBook-Pro.local>
2024-10-25 11:55:29 +0530

    Cloud Resume Project Index Page

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
